8fd5f211973aaa058a3f06f064582c8c8b20a0bc
Iori205 <b.sodbilegt11@gmail.com>
2025-09-23 11:14:30 +0800

    See-more/Repair

--- movie/app/See-more/page.tsx
+++ movie/app/See-more/page.tsx
@@ -10,22 +10,34 @@ type MorePageProps = {
   const params = await searchParams;
   const title = params.title;
 
+
   const moviesRes: movieResponseType = await getMoviesList(title);
   console.log(moviesRes)
+
+  let headerTitle = ""
+  if(title === "upcoming"){
+    headerTitle = "Upcoming"
+  }else if(title === "top_rated"){
+    headerTitle = "Top Rated"
+  }else{
+    headerTitle = "Popular"
+  }
   return (
-    <div>
-      <h2 className="text-3xl font-bold">{title}</h2>
-      <div className="flex gap-4 flex-wrap">
-      {moviesRes.results.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          id={movie.id}
-          title={movie.title}
-          score={movie.vote_average}
-          image={movie.poster_path}
-        />
-      ))}
-    </div>
+    <div className="max-w-[1280px] m-auto">
+      <div className="flex flex-col justify-center">
+        <h2 className="text-3xl font-bold">{headerTitle}</h2>
+        <div className="flex gap-4 flex-wrap">
+          {moviesRes.results.map((movie) => (
+          <MovieCard
+           key={movie.id}
+           id={movie.id}
+           title={movie.title}
+           score={movie.vote_average}
+           image={movie.poster_path}
+           />
+          ))}
+        </div>
+      </div>  
     </div>
   );
 };
